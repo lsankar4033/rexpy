e8978030e4ee940880400b2c951665b6b566ef90
lsankar4033 <lsankar4033@gmail.com>
2016-11-27 16:48:21 -0500

    Add simple RE -> NFA logic

--- src/nfa.py
+++ src/nfa.py
@@ -1,3 +1,45 @@
+class NFA:
+    """The NFA class represents an entire NFA representing an RE. Each NFA consists of a start_node and
+    accept_nodes. The transition function is implicit as each node has its own transition map.
+    """
+    def __init__(self, start_node, accept_nodes):
+        self.start_node = start_node
+        self.accept_nodes = accept_nodes
+
+    def __str__(self):
+        """Human readable format used just for debugging.
+        """
+
+        # Use 'id(x)' to label each node
+        edges = []
+
+        # Do a dfs through the graph, keeping track of each edge
+        frontier = [self.start_node]
+        visited = set()
+        node_dict = {} # used to re-index nodes to a more human readable format
+        cur_node_ind = 0
+        while len(frontier) > 0:
+            next_node = frontier.pop()
+            node_dict[id(next_node)] = cur_node_ind
+            cur_node_ind += 1
+
+            for edge, nodes in next_node.transitions.items():
+                for node in nodes:
+                    edges.append((id(next_node), edge, id(node)))
+
+                    if node not in visited:
+                        frontier.append(node)
+
+            visited.add(next_node)
+
+        lines = [str(node_dict[s]) + " -> '" + str(t) + "' -> " + str(node_dict[e]) for s,t,e in edges]
+        lines_str = "Edges: \n" + "\n".join(lines)
+
+        start_str = "Start node: " + str(node_dict[id(self.start_node)])
+        accept_str = "Accept nodes: " + str([node_dict[id(n)] for n in self.accept_nodes])
+
+        return "\n".join([start_str, accept_str, lines_str])
+
 class Node:
     """The Node class represents a single State of the NFA as well as any transitions out of it. Nodes are
     represented this way to facilitate BFS through the NFA.
@@ -6,33 +48,42 @@ class Node:
     """
     def __init__(self):
         self.transitions = {}
-        self.is_accept = False
 
     def add_transition(self, trans_char, node):
-        if self.transitions.has_key(trans_char):
+        if trans_char in self.transitions:
             self.transitions[trans_char].append(node)
         else:
             self.transitions[trans_char] = [node]
 
 class NodeGroup:
     """A NodeGroup represents a single grouping from the RE derivation tree. Each NodeGroup must have both a
-    start Node and an end Node so that it can be composed with other groups properly.
+    start Node and an end Node so that it can be composed with other groups properly while building the NFA
+    from an RE grammar derivation.
     """
     def __init__(self, start_node, end_node):
         self.start_node = start_node
         self.end_node = end_node
 
+def build_single_char_node_group(char):
+    """Given a single character, creates a node group with one transition from start node to end node using
+    that character."""
+    start = Node()
+    end = Node()
+    start.add_transition(char, end)
+    return NodeGroup(start, end)
+
 def build_star_node_group(input_group):
-    """Given an NodeGroup input_group, create a new NodeGroup representing (input_group)*."""
+    """Given a NodeGroup input_group, create a new NodeGroup representing (input_group)*."""
     new_start = Node()
     new_start.add_transition("", input_group.start_node)
 
     new_end = Node()
     input_group.end_node.add_transition("", new_end)
+    new_end.add_transition("", new_start)
 
     return NodeGroup(new_start, new_end)
 
-def build_concat_node_group(*input_groups):
+def build_concat_node_group(input_groups):
     """Given a sequence of NodeGroups, create a NodeGroup representing their (in order) concatenation."""
     for i in range(1, len(input_groups)):
         input_groups[i-1].end_node.add_transition("", input_groups[i].start_node)
@@ -41,6 +92,6 @@ def build_concat_node_group(*input_groups):
     new_end = Node()
 
     new_start.add_transition("", input_groups[0].start_node)
-    input_groups[-1].add_transition("", new_end)
+    input_groups[-1].end_node.add_transition("", new_end)
 
     return NodeGroup(new_start, new_end)
--- src/re_parser.py
+++ src/re_parser.py
@@ -1,4 +1,4 @@
-
+import nfa
 
 # TODO When the grammar is augmented to have more than just '*' and 'concat,' change this to properly parse a
 # CFG and return a tree.
@@ -24,7 +24,25 @@ def parse_re_string(re_str):
 
     return re_terminals
 
+# This method is a temporary approach to converting a terminal string (i.e. a or a*) to a node group. Once I
+# update the parsed form of an RE to include the full derivation tree, this method should no longer be
+# necessary.
+def term_str_to_node_group(term_str):
+    if len(term_str) is 1:
+        return nfa.build_single_char_node_group(term_str[0])
+    elif len(term_str) is 2 and term_str[1] is "*":
+        base = nfa.build_single_char_node_group(term_str[0])
+        return nfa.build_star_node_group(base)
+    else:
+        raise ValueError("Term string %s isn't of the form 'a' or 'a*'" % term_str)
+
 # TODO Change the input of this function to be a tree of nodes instead of just an array representing concat.
 # Convert a parsed RE to the NFA that recognizes it.
-def convert_re_to_nfa(re):
-    pass
+def convert_re_to_nfa(parsed_re):
+    to_concat = [term_str_to_node_group(t) for t in parsed_re]
+    concat_node_group = nfa.build_concat_node_group(to_concat)
+
+    return nfa.NFA(concat_node_group.start_node, [concat_node_group.end_node])
+
+def re_string_to_nfa(re_str):
+    return convert_re_to_nfa(parse_re_string(re_str))
